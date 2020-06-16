135510fb54bf5421f17141724d60a74dbf852726
Chirpatel <32012234+Chirpatel@users.noreply.github.com>
2020-06-17 02:00:33 +0530

    v4

--- controllers/shop.js
+++ controllers/shop.js
@@ -23,11 +23,10 @@ exports.getIndex = (req,res,next)=>{
 }
 
 exports.getCart = (req,res,next)=>{
-  res.render('shop/cart'),{
+  res.render('shop/cart',{
     path : '/cart',
-    pageTitle : "Your Cart"
-  }
- 
+    pageTitle : "Your Cart",
+  });
 }
 
 
@@ -35,5 +34,5 @@ exports.getCheckout = (req,res,next)=>{
   res.render('shop/checkout',{
     path:'/checkout',
     pageTitle:'Checkout'
-  })
+  });
 }
